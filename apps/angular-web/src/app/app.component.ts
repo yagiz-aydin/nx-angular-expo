0964f1949a7249f6addddc8838d87d72bccde040
yagizaydin <yagiz.aydin@outlook.com.tr>
2025-09-15 21:15:40 +0300

    [main] structural init

---
+++ apps/angular-web/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Greet } from '../../../../packages/shared';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  title = 'angular-web';
+  ngOnInit(){
+    console.log(Greet('Yağız'));
+  }
+}
---
+++ apps/expo-mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { useColorScheme } from '@/apps/expo-mobile/hooks/use-color-scheme';
+import { Colors } from '@/apps/expo-mobile/constants/theme';
+import { HapticTab } from '@/apps/expo-mobile/components/haptic-tab';
+import { IconSymbol } from '@/apps/expo-mobile/components/ui/icon-symbol.ios';
+
+import { Greet } from '@/packages/shared/greet';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  
+  Greet("Yağız")
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
